a27f2fff27b9e7f7ff498490896816407e126adc
melike3434 <gedenemelike@gmail.com>
2026-07-13 17:04:07 +0300

    Move tsconfig files back to PhoneBook.Web

--- PhoneBook.Core/obj/Debug/net8.0/PhoneBook.Core.AssemblyInfo.cs
+++ PhoneBook.Core/obj/Debug/net8.0/PhoneBook.Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PhoneBook.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c10ee840cc6ab0a3fe4d6d64018e54fbfc90afe3")]
 [assembly: System.Reflection.AssemblyProductAttribute("PhoneBook.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PhoneBook.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
